9a68c57563c8411173c3cdf7c07bfb7e73352038
eliessenouci <elies.senouci@gmail.com>
2020-10-08 22:21:10 +0200

    favorite button interactive

--- lib/main.dart
+++ lib/main.dart
@@ -36,12 +36,7 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-        /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
+        Favorite()
       ],
     ),
   );
@@ -116,5 +111,52 @@ class MyApp extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class Favorite extends StatefulWidget {
+  @override
+  _FavoriteState createState() => _FavoriteState();
+}
+
+class _FavoriteState extends State<Favorite> {
+  bool _isFav = false;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFav ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFav) {
+        _favoriteCount -= 1;
+        _isFav = false;
+      } else {
+        _favoriteCount += 1;
+        _isFav = true;
+      }
+    });
   }
 }
